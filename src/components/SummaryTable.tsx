250303ef5a83291fb2dca0f5b73bfe27b5f67145
Daniel Moura <daniel-moura@outlook.com>
2023-02-03 15:23:36 -0300

    fix: quantity of habit days

--- src/components/SummaryTable.tsx
+++ src/components/SummaryTable.tsx
@@ -5,7 +5,7 @@ const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S']
 
 const summaryDates = generateDatesFromYearBeginning()
 
-const minimumSummayDatesSize = (18 * 7) - 1
+const minimumSummayDatesSize = 18 * 7
 const amountOfDaysToFill = minimumSummayDatesSize - summaryDates.length
 
 export function SummaryTable() {
@@ -22,7 +22,6 @@ export function SummaryTable() {
       </div>
 
       <div className='grid grid-rows-7 grid-flow-col gap-3'>
-        <HabitDay />
         {summaryDates.map(date => {
           return <HabitDay key={date.toString()} />
         })}
