b6c28466612ebbf55801748228e029fb4d467e71
Daniel Moura <daniel-moura@outlook.com>
2023-02-05 02:10:38 -0300

    fix: progress bar

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -7,7 +7,7 @@ export function ProgressBar(props: ProgressBar) {
     <div className='h-3 rounded-xl bg-zinc-700 w-full mt-4'>
       <div 
         role='progressbar'
-        aria-aria-label='Progresso de hábitos completados no dia'
+        aria-label='Progresso de hábitos completados no dia'
         aria-valuenow={props.progress}
         className='h-3 rounded-xl bg-violet-600 w-3/4'
         style={{
