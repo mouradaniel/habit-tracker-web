4be59469895803dfaa596ef265a6c85caa8b20b1
Daniel Moura <daniel-moura@outlook.com>
2023-02-11 16:10:42 -0300

    refactor: removing footer on app

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import './styles/global.css';
 import './lib/dayjs';
 import { Header } from './components/Header';
 import { SummaryTable } from './components/SummaryTable';
-import { Footer } from './components/Footer';
 
 export function App() {
   return (
@@ -10,7 +9,6 @@ export function App() {
       <div className='w-full max-w-5xl px-6 flex flex-col gap-16'>
         <Header />
         <SummaryTable />
-        <Footer />
       </div>
     </div>
   )    
